c872eef14377b8fca9feffce3e715bf96027bce9
andre-frr <220001208@esg.ipsantarem.pt>
2024-10-24 16:28:32 +0100

    Otimizações gerais

--- WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55a4361cda085f0dddfa83ab6c42334d49ab86cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ade1f843d85da02d5737a1d1f5d93e0ce2c4b7c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
