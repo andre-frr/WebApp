a45eeb135d9833a5a4a710287168ba42b0e5db55
andre-frr <220001208@esg.ipsantarem.pt>
2024-10-25 18:06:45 +0100

    Adicionada a página do evento "Fica Comigo Esta Noite"; adicionada a pagina do evento "A Morte do Corvo"; melhorias e otimizações.

--- WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91e9b7227bdc35b99bd437a93cbf0b035d471d32")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ddda5b43fc4a12aa93c4997db0a6fb4ab19681b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
