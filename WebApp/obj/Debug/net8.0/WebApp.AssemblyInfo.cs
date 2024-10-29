0965851476d52fe5a0985453e3a256eb0a5ee519
andre-frr <220001208@esg.ipsantarem.pt>
2024-10-29 12:55:21 +0000

    Draft da página de pagamento; Melhorias.

--- WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69f6a2a4aa11c1f473c635bab7f5936017130d6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae73f723fbf5cba3863d3c4f9553744070358a0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
