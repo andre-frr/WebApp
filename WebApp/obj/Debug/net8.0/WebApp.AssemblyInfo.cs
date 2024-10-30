5474352c8c933d81520ceafb161fa45c37e8e16c
cigonha1 <220000891@esg.ipsantarem.pt>
2024-10-30 15:18:42 +0000

    Draft da página de prefil do utilizador.

--- WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1638dc80d16625cb288be9503f6081e5a05f856")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36fd81aa544afb7bd39ddc1fb1792ca2b729a666")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
