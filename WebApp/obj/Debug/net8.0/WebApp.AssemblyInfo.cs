29dde3899cd908a1101c88022649b95cec7284bd
andre-frr <220001208@esg.ipsantarem.pt>
2024-11-01 11:02:54 +0000

    Mais melhorias à página do perfil;

--- WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5474352c8c933d81520ceafb161fa45c37e8e16c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+803315add7c75d966a479319239640952ba8cbbc")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
