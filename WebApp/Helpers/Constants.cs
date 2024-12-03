0d6717bdfaaf7760d4d852e11af6732883a26f1a
cigonha1 <220000891@esg.ipsantarem.pt>
2024-12-03 16:07:19 +0000

    Draft do Fluxo do Home

--- WebApp/Helpers/Constants.cs
+++ WebApp/Helpers/Constants.cs
@@ -4,5 +4,8 @@
     {
         //Utilizador
         public const string sp_utilizador_insert = "sp_utilizador_insert";
+
+        //Eventos
+        public const string sp_evento_get = "sp_evento_get";
     }
 }
---
+++ WebApp/Services/HomeService.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Options;
+using System.Data;
+using WebApp.Helpers;
+using WebApp.Models.DTOs;
+
+namespace WebApp.Services
+{
+    public class HomeService
+    {
+        private readonly MyOptions _myOptions;
+        public HomeService(IOptions<MyOptions> myOptions)
+        {
+            _myOptions = myOptions.Value;
+        }
+        public ExecutionResult<eventoDTO> Get(int id)
+        {
+            eventoDTO evento = null;
+
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add("@id", id, DbType.Int32, ParameterDirection.Input);
+
+            using (IDbConnection conn = new SqlConnection(_myOptions.ConnString))
+            {
+                evento = conn.QueryFirstOrDefault<eventoDTO>(Constants.sp_evento_get,parameters,commandType: CommandType.StoredProcedure);
+            }
+             return new ExecutionResultFactory<utilizadorDTO>().GetSuccessExecutionResult(id, string.Empty);
+        }
+    }
+}
+
