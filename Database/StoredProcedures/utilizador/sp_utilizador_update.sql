abe95c6cd0b937f21e42104bf5365e1fcf63c045
André Ferreira <220001208@esg.ipsantarem.pt>
2024-12-10 16:40:39 +0000

    Adicionado SP para atualizar a info do utilizador

---
+++ Database/StoredProcedures/utilizador/sp_utilizador_update.sql
@@ -0,0 +1,24 @@
+﻿create procedure [dbo].[sp_utilizador_update]
+	@userID int,
+	@nome nvarchar(max),
+	@apelido nvarchar(max),
+	@email nvarchar(max),
+	@dt_nascimento date,
+	@morada nvarchar(max),
+	@nif int,
+	@cidade nvarchar(max),
+	@cod_postal char(8)
+as
+begin
+	update utilizador
+	set
+		nome = @nome,
+		apelido = @apelido,
+		email = @email,
+		dt_nascimento = @dt_nascimento,
+		morada = @morada,
+		nif = @nif,
+		cidade = @cidade,
+		cod_postal = @cod_postal
+	where userID = @userID
+end
