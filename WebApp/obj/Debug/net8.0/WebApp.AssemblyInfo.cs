d731de18659edc44bd9bf02a48f9d958ce3319ea
André Ferreira <220001208@esg.ipsantarem.pt>
2024-12-11 16:08:17 +0000

    Connection String atualizada para servidor em Azure

--- WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13afab79fcf289d1d5d90eeee09a485f33e0617f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00c3c49181291e1a8332c8bc299cdce5836f35c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
