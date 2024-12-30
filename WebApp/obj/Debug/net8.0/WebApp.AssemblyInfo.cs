a17b93ee4420a7f054037dcb402ba8fa5f3f1358
André Ferreira <220001208@esg.ipsantarem.pt>
2024-12-30 18:55:47 +0000

    Debug (1/3)

--- WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe5d93cf7d621c75aa0111b37b96ce6530805c19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fe3e41afc211e062c43b40ead7cc27c1c5b6131")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
